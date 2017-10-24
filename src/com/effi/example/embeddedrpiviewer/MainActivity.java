1abced1b39fe8dad966c14178e78a0a78a1848c3
Robbie Small <robsmall@umich.edu>
2017-10-23 20:32:41 -0400

    Clean up whitespace and style around MainActivity code

--- src/com/effi/example/embeddedrpiviewer/MainActivity.java
+++ src/com/effi/example/embeddedrpiviewer/MainActivity.java
@@ -14,82 +14,83 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+  private final static String PREVIEW_INTENT_KEY = "pl.effisoft.rpicamviewer2.PREVIEW";
+  private final static int camera = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		final EditText editText1 = (EditText) findViewById(R.id.editText1);
-		Button button1 = (Button) findViewById(R.id.button1);
-		button1.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				Intent intent = new Intent("pl.effisoft.rpicamviewer2.PREVIEW");
-				int camera =0;
-				
-				//--------- Basic settings
-				intent.putExtra("full_screen", true);
-				
-				intent.putExtra("name"+camera, "My pipeline name");
-				intent.putExtra("host"+camera, "192.168.0.1");
-				intent.putExtra("port"+camera, 5000);
-				intent.putExtra("description"+camera, "My pipeline description");
-				intent.putExtra("uuid"+camera, UUID.randomUUID().toString() );
-				intent.putExtra("aspectRatio"+camera, 1.6);
-				intent.putExtra("autoplay"+camera, true);
-				
-				//--------- Enable advanced mode
-				intent.putExtra("advanced"+camera, true);
-				intent.putExtra("custom_pipeline"+camera, editText1.getText().toString());
-				
-				//--------- Enable application extra features
-				intent.putExtra("extraFeaturesEnabled"+camera, false);
-				
-				//--------- Add autoaudiosink to featured pipeline
-				intent.putExtra("extraFeaturesSoundEnabled"+camera, false);
-				
-				//--------- Scale Video Stream option
-				intent.putExtra("extraResizeVideoEnabled"+camera, false);
-				intent.putExtra("width"+camera, 320);
-				intent.putExtra("height"+camera, 200);
-				
-				//--------- Add plugins
-				ArrayList<String> plugins = new ArrayList<String>();
-				intent.putExtra("plugins"+camera, plugins);
-				
-				intent.setPackage("pl.effisoft.rpicamviewer2");
-		        startActivityForResult(intent, 0);
-			}
-		});
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-	    if (requestCode == 0) {
-	        if (resultCode == RESULT_OK) {
+    final EditText editText1 = (EditText) findViewById(R.id.editText1);
+    Button button1 = (Button) findViewById(R.id.button1);
+    button1.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View arg0) {
+        Intent intent = new Intent(PREVIEW_INTENT_KEY);
 
-	        } else if (resultCode == RESULT_CANCELED) {
+        //--------- Basic settings
+        intent.putExtra("full_screen", true);
 
-	        }
-	    }
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        intent.putExtra("name" + camera, "My pipeline name");
+        intent.putExtra("host" + camera, "192.168.0.1");
+        intent.putExtra("port" + camera, 5000);
+        intent.putExtra("description" + camera, "My pipeline description");
+        intent.putExtra("uuid" + camera, UUID.randomUUID().toString());
+        intent.putExtra("aspectRatio" + camera, 1.6);
+        intent.putExtra("autoplay" + camera, true);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        //--------- Enable advanced mode
+        intent.putExtra("advanced" + camera, true);
+        intent.putExtra("custom_pipeline" + camera, editText1.getText().toString());
+
+        //--------- Enable application extra features
+        intent.putExtra("extraFeaturesEnabled" + camera, false);
+
+        //--------- Add autoaudiosink to featured pipeline
+        intent.putExtra("extraFeaturesSoundEnabled" + camera, false);
+
+        //--------- Scale Video Stream option
+        intent.putExtra("extraResizeVideoEnabled" + camera, false);
+        intent.putExtra("width" + camera, 320);
+        intent.putExtra("height" + camera, 200);
+
+        //--------- Add plugins
+        ArrayList<String> plugins = new ArrayList<String>();
+        intent.putExtra("plugins" + camera, plugins);
+
+        intent.setPackage("pl.effisoft.rpicamviewer2");
+        startActivityForResult(intent, 0);
+      }
+    });
+  }
+
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    if (requestCode == 0) {
+      if (resultCode == RESULT_OK) {
+
+      } else if (resultCode == RESULT_CANCELED) {
+
+      }
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
 }
